eea76c94ae5402ebdb58be8f9390e6dc4bba33b6
Fred <abdullaevfarhodjon@gmai.com>
2024-05-21 20:46:44 +0900

    2

---
+++ LeetCodeCsharp/Top150/238_ProductOfArrayExceptSelf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150
+{
+    internal class _238_ProductOfArrayExceptSelf
+    {
+
+    }
+}
---
+++ LeetCodeCsharp/Top150/380_InsertDeleteGetRandom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150;
+//Medium
+//44 minutes, because i keep trying to find solution by combination of list
+//and dict but dict was enough for solving
+public class RandomizedSet
+{
+    Random rnd = new Random();
+    private readonly Dictionary<int, int> dict;
+
+    public RandomizedSet()
+    {
+        dict = new Dictionary<int, int>();
+    }
+
+    public bool Insert(int val)
+    {
+        return dict.TryAdd(val, val);
+    }
+
+    public bool Remove(int val)
+    {
+        return dict.Remove(val);
+    }
+
+    public int GetRandom()
+    {
+        
+        int index = rnd.Next(dict.Count);
+        return dict.ElementAtOrDefault(index).Key;
+    }
+}
+
+
