3c819705e09c647d7f99c10b0b66d33ad7c4f1dd
Fred <abdullaevfarhodjon@gmai.com>
2024-05-23 21:25:52 +0900

    hard one

--- LeetCodeCsharp/Program.cs
+++ LeetCodeCsharp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeCsharp.Top150;
+
 Console.WriteLine("Hello, World!");
+_68_TextJustification.Run();
+Console.ReadLine();
---
+++ LeetCodeCsharp/Top150/28_FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150;
+//Easy
+internal class _28_FindTheIndexOfTheFirstOccurrenceInAString
+{
+    //36 minutes
+    public int StrStr(string haystack, string needle)
+    {
+        if (haystack.Length < needle.Length) return -1;
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] != needle[0]) continue;
+            if (haystack.Substring(i, haystack.Length - i).StartsWith(needle)) return i;
+        }
+        return -1;
+    }
+
+    //optimation
+    public int StrStr2(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
