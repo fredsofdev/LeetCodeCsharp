0375d0eaff38f48a343850e50aac81067eadc65e
Fred <abdullaevfarhodjon@gmai.com>
2024-06-02 19:40:48 +0900

    w

---
+++ LeetCodeCsharp/Top150/21 Merge Two Sorted Lists.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150;
+//Easy
+internal class _21_Merge_Two_Sorted_Lists
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    //19 minutes
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode pointer = new ListNode();
+        ListNode head = pointer;
+        while(list1 != null || list2 != null)
+        {
+            int val1 = list1 == null ? int.MaxValue : list1.val;
+            int val2 = list2 == null ? int.MaxValue : list2.val;
+            if (val1 > val2) list2 = list2.next;
+            else list1 = list1.next;
+
+            var newNode = new ListNode(Math.Min(val1,val2));
+            pointer.next = newNode;
+            pointer = newNode;
+        }
+        return head.next;
+    }
+
+    // Without creating new ListNode
+    public ListNode MergeTwoLists2(ListNode list1, ListNode list2)
+    {
+        ListNode pointer = new ListNode();
+        ListNode head = pointer;
+        while (list1 != null && list2 != null)
+        {
+            if (list1?.val > list2?.val)
+            {
+                pointer.next = list2;
+                list2 = list2.next;
+            }
+            else
+            {
+                pointer.next = list1;
+                list1 = list1.next;
+            }
+            Console.WriteLine(pointer.val);
+            pointer = pointer.next;
+        }
+
+        if (list1 == null) pointer.next = list2;
+        else pointer.next = list1;
+
+        return head.next;
+    }
+}
---
+++ LeetCodeCsharp/Top150/224_Basic Calculatorr.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150;
+
+internal class _224_Basic_Calculatorr
+{
+    public int Calculate(string s)
+    {
+        s  = s.Trim(' ');
+        Stack<string> stack = new();
+        Stack<char> oper = new();
+        int i = 0;
+        while(i < s.Length)
+        {
+            if (s[i] == ' ')
+            {
+                i++;
+                continue;
+            }
+            if (char.IsDigit(s[i]))
+            {
+                string num = "";
+                int dId = i;
+                while (dId < s.Length && char.IsDigit(s[dId])) num += s[dId++];
+                if(stack.TryPeek(out string n) && int.TryParse(n,out int v1))
+                {
+                    int v2 = int.Parse(num);
+                    char op = oper.Pop();
+                    int res = 0;
+                    if (op == '+') res = v1 + v2;
+                    else if (op == '-') res = v1 - v2;
+                    else if (op == '*') res = v1 * v2;
+                    else res = v1 / v2;
+                    stack.Push(res.ToString());
+                }
+                else stack.Push(num);
+                i = dId;
+            }
+            else if (s[i] != ')' && s[i] != '(') oper.Push(s[i]);
+            else if (s[i] == '(') stack.Push("" + s[i]);
+            else
+            {
+                string hold = stack.Pop();
+                stack.Pop();
+                stack.Push(hold);
+            }
+            i++;       
+        }
+
+        while(oper.Count > 0)
+        {
+            int v2 = int.Parse(stack.Pop());
+            int v1 = int.Parse(stack.Pop());
+            char op = oper.Pop();
+            int res = 0;
+            if (op == '+') res = v1 + v2;
+            else if (op == '-') res = v1 - v2;
+            else if (op == '*') res = v1 * v2;
+            else res = v1 / v2;
+            stack.Push(res.ToString());
+        }
+
+        return int.Parse(stack.Pop());
+        
+    }
+
+    public int Calculate2(string s)
+    {
+        s = s.Trim(' ');
+        List<string> cal = new List<string>();
+        bool sign = false;
+        int i = 0;
+        while (i < s.Length)
+        {
+            if (s[i] == ' ')
+            {
+                i++;
+                continue;
+            }
+            string num = sign? "-": "";
+            if (char.IsDigit(s[i]))
+            {
+                int dId = i;
+                while (dId < s.Length && char.IsDigit(s[dId])) num += s[dId++];
+                cal.Add(num);
+                i = dId;
+                sign = false;
+            }
+            else if (s[i] == '-' && cal.Last() == "(")
+            {
+                sign = true;
+            }
+            else cal.Add("" + s[i]);
+            i++;
+        }
+        Console.WriteLine(string.Join(",", cal));
+        return 0;
+    }
+
+}
