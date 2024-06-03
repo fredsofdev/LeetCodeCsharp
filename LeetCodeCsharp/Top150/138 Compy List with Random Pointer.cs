b3e89f34c8afa0c2d457c1286f88e6caabd3fc03
abdulFred <abdullaevfarhodjon@gmail.com>
2024-06-03 17:41:38 +0900

    138

--- LeetCodeCsharp/Top150/138 Compy List with Random Pointer.cs
+++ LeetCodeCsharp/Top150/138 Compy List with Random Pointer.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LeetCodeCsharp.Top150;
-
+//Medium
 internal class _138_Compy_List_with_Random_Pointer
 {
-
+    // 60 minutes
     public class Node
     {
         public int val;
@@ -25,36 +25,24 @@ internal class _138_Compy_List_with_Random_Pointer
 
     public Node CopyRandomList(Node head)
     {
-        if (head == null) return null;
-        Node copy = new Node(head.val);
-        Node copyP = copy;
-        Node curr = head.next;
+        Dictionary<Node, Node> pair = new();
+        Node copy = new Node(0);
+        Node currCopy = copy;
+        Node curr = head;
         while (curr != null)
         {
-            copyP.next = new Node(curr.val);
-            copyP = copyP.next;
+            currCopy.next = new Node(curr.val);
+            pair[curr] = currCopy.next;
+            currCopy = currCopy.next;
             curr = curr.next;
         }
 
-        curr = copy;
-        while (curr != null || head != null)
+        foreach (var p in pair)
         {
-
-            if (head.random == null)
-            {
-                head = head.next;
-                curr = curr.next;
-                continue;
-            }
-            Console.WriteLine(head.random.val);
-            Node find = curr;
-            while (head.random.val != find.val)
-                find = find.next;
-            
-            curr.random = find;
-            head = head.next;
-            curr = curr.next;
+            if (p.Key.random == null) continue;
+            if (pair.TryGetValue(p.Key.random, out Node? random)) p.Value.random = random;
         }
-        return copy;
+
+        return copy.next;
     }
 }
