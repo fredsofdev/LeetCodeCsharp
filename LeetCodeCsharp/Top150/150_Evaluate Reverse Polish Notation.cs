9e6d61bf98ef310814497049d15cdf756775dbde
abdulFred <abdullaevfarhodjon@gmail.com>
2024-06-03 16:15:28 +0900

    150

---
+++ LeetCodeCsharp/Top150/150_Evaluate Reverse Polish Notation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150;
+//Medium
+internal class _150_Evaluate_Reverse_Polish_Notation
+{
+    //16 minutes
+    public int EvalRPN(string[] tokens)
+    {
+        Stack<int> operands = new();
+
+        foreach (var token in tokens)
+        {
+            if (int.TryParse(token, out int val))
+            {
+                operands.Push(val);
+                continue;
+            }
+
+            int op2 = operands.Pop();
+            int op1 = operands.Pop();
+            if (token == "+") operands.Push(op1 + op2);
+            else if (token == "-") operands.Push(op1 - op2);
+            else if (token == "*") operands.Push(op1 * op2);
+            else operands.Push(op1 / op2);
+        }
+        return operands.Pop();
+    }
+}
---
+++ LeetCodeCsharp/Top150/224 Basic Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Top150;
+
+internal class _224_Basic_Calculator
+{
+    public int Calculate(string s)
+    {
+        Stack<string> oper = new Stack<string>();
+        List<string> valid = new();
+        string num = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            
+            if (!isLast && s[i] == ' ') continue;
+            if (char.IsDigit(s[i]))
+            {
+                num += s[i];
+                if (isLast)
+                {
+                    valid.Add(num);
+                }
+                continue;
+            }
+
+            if(s[i] != '-')
+            {
+                valid.Add(num);
+            }
+
+            if (num.Length > 0)
+            {
+                if (s[i] != '-') valid.Add("-"+num);
+                else  valid.Add(num);
+            }
+
+            if (s[i] != ' ')
+            {
+                if (s[i] != '-') valid.Add("-");
+                else  valid.Add("" + s[i]);
+            }
+            num = "";
+        }
+       
+        return int.Parse(oper.Pop());
+    }
+}
