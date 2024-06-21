794d3e1cb36162b785dd84cb685637d54fd978b0
abdulFred <abdullaevfarhodjon@gmail.com>
2024-06-21 17:21:22 +0900

    1448

---
+++ LeetCodeCsharp/Bliz 75/1448 Count Good Nodes in Binary Tree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Bliz_75;
+//Medium
+internal class _1448_Count_Good_Nodes_in_Binary_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //17 minutes
+    public int GoodNodes(TreeNode root)
+    {
+        int count = 0;
+        Count(root,int.MinValue, ref count);
+        return count;
+    }
+
+    private void Count (TreeNode root, int max, ref int count)
+    {
+        if (root == null) return;
+        if (root.val >= max) count++;
+
+        Count(root.left, Math.Max(root.val, max),ref count);
+        Count(root.right, Math.Max(root.val, max), ref count);
+    }
+}
---
+++ LeetCodeCsharp/Bliz 75/2130 Maximum Twin Sum of a Linked List.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp.Bliz_75;
+//Medium
+internal class _2130_Maximum_Twin_Sum_of_a_Linked_List
+{
+    //10 minutes
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public int PairSum(ListNode head)
+    {
+        int max = int.MinValue;
+        Stack<int> half = new Stack<int>();
+        ListNode fast = head;
+        var slow = head;
+        while (fast != null)
+        {
+            half.Push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        while(slow != null)
+        {
+            max = Math.Max(max, slow.val + half.Pop());
+            slow = slow.next;
+        }
+        return max;
+    }
+}
